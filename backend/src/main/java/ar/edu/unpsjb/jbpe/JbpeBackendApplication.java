214c0f48f75321ae22b492946888825dc358bb5b
DobleTmatutin0 <zz_00024@hotmail.com>
2026-07-29 15:54:33 -0300

    add backend basic structure

---
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/JbpeBackendApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unpsjb.jbpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbpeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbpeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ar/edu/unpsjb/jbpebackend/JbpeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unpsjb.jbpebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbpeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
