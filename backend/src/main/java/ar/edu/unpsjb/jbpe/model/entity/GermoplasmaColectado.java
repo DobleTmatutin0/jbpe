1dd0c89f5d7e2d12bb7e5339f0ce40ea4983578f
DobleTmatutin0 <zz_00024@hotmail.com>
2026-08-18 20:57:42 -0300

    adds germoplasmaColectado's model

---
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/model/entity/GermoplasmaColectado.java
@@ -0,0 +1,31 @@
+package ar.edu.unpsjb.jbpe.model.entity;
+
+import ar.edu.unpsjb.jbpe.model.enumeration.TipoDeGermoplasma;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "germoplasma_colectado")
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class GermoplasmaColectado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "adquisicion_id", nullable = false)
+    private Ejemplar ejemplar;
+
+    @Column(name = "tipo_germoplasma", nullable = false)
+    private TipoDeGermoplasma tipoDeGermoplasma;
+}
