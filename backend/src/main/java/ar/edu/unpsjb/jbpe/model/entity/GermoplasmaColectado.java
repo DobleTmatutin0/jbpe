17c167a1c583c53ce0b31cf28162b1ef31cf6014
DobleTmatutin0 <zz_00024@hotmail.com>
2026-08-20 18:07:45 -0300

    add JoinColumn at ejemplar attribute

--- backend/src/main/java/ar/edu/unpsjb/jbpe/model/entity/GermoplasmaColectado.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/model/entity/GermoplasmaColectado.java
@@ -6,7 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,7 +26,8 @@ public class GermoplasmaColectado {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "adquisicion_id", nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "adquisicion_id", nullable = false)
     private Ejemplar ejemplar;
 
     @Column(name = "tipo_germoplasma", nullable = false)
