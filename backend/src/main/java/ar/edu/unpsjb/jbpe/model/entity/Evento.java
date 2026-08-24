893a63850f603ab127ee0c8f7e95da63d75efd9c
DobleTmatutin0 <zz_00024@hotmail.com>
2026-08-24 19:29:09 -0300

    change id generationType from Auto to Identity

--- backend/src/main/java/ar/edu/unpsjb/jbpe/model/entity/Evento.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/model/entity/Evento.java
@@ -25,7 +25,7 @@ import lombok.Setter;
 
 public class Evento {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "tipo_evento", nullable = false)
