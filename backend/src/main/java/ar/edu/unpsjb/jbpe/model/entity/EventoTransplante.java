79e5d1c39fa56a878da486aa4585755a6e171373
DobleTmatutin0 <zz_00024@hotmail.com>
2026-08-24 20:06:51 -0300

    add EventoTransplante entity

---
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/model/entity/EventoTransplante.java
@@ -0,0 +1,41 @@
+package ar.edu.unpsjb.jbpe.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "evento_transplante")
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class EventoTransplante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Evento evento;
+
+    @ManyToOne
+    @JoinColumn(name = "sector_inicial", nullable = false)
+    private Sector sectorInicial;
+
+    @ManyToOne
+    @JoinColumn(name = "transplantado_por_id", nullable = false)
+    private Persona transplantadoPor;
+
+    private String ComoFuePlantadoEnPredio;
+
+}
