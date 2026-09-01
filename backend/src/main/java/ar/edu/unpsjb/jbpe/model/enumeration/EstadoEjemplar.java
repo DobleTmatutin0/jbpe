1b377f388571a81286e515d75898dcf11dc9e102
DobleTmatutin0 <zz_00024@hotmail.com>
2026-09-01 09:29:38 -0300

    changes the format. now the enum data adjust with the db data

--- backend/src/main/java/ar/edu/unpsjb/jbpe/model/enumeration/EstadoEjemplar.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/model/enumeration/EstadoEjemplar.java
@@ -2,8 +2,19 @@ package ar.edu.unpsjb.jbpe.model.enumeration;
 
 // estado actual
 public enum EstadoEjemplar {
-    INDEFINIDO,
-    ADQUISICION,
-    ACCESION,
-    DESACCESION
+    INDEFINIDO("indefinido"),
+    ADQUISICION("adquisicion"),
+    ACCESION("accesionado"),
+    DESACCESION("desaccesionado");
+
+    private final String sqlValue;
+
+    EstadoEjemplar(String sqlValue) {
+        this.sqlValue = sqlValue;
+    }
+
+    public String getSqlValue() {
+        return sqlValue;
+    }
+
 }
--- backend/src/main/java/ar/edu/unpsjb/jbpe/model/enumeration/LocationLevel.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/model/enumeration/LocationLevel.java
@@ -1,8 +1,19 @@
 package ar.edu.unpsjb.jbpe.model.enumeration;
 
 public enum LocationLevel {
-    COUNTRY,
-    STATEPROVINCE, // cambiar por state_provinces
-    COUNTY,
-    LOCALITY
+    COUNTRY("country"),
+    STATE_PROVINCE("stateprovince"),
+    COUNTY("county"),
+    LOCALITY("locality");
+
+    private final String sqlValue;
+
+    LocationLevel(String sqlValue) {
+        this.sqlValue = sqlValue;
+    }
+
+    public String getSqlValue() {
+        return sqlValue;
+    }
+
 }
