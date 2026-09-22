4cfdb38c55cb738ac5cf29f7516f13f3d2f4a8e3
DobleTmatutin0 <zz_00024@hotmail.com>
2026-09-22 18:29:15 -0300

    add save method

--- backend/src/main/java/ar/edu/unpsjb/jbpe/business/service/EjemplarService.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/business/service/EjemplarService.java
@@ -3,18 +3,26 @@ package ar.edu.unpsjb.jbpe.business.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.unpsjb.jbpe.business.repository.EjemplarRepository;
+import ar.edu.unpsjb.jbpe.business.repository.SitioDeRecoleccionRepository;
 import ar.edu.unpsjb.jbpe.model.dto.EjemplarDTO;
 import ar.edu.unpsjb.jbpe.model.dto.EjemplarMinDTO;
+import ar.edu.unpsjb.jbpe.model.entity.Ejemplar;
 
 @Service
 
 public  class EjemplarService {
     private final EjemplarRepository ejemplarRepository;
+    private final SitioDeRecoleccionRepository sitioDeRecoleccionRepository;
 
-    public EjemplarService(EjemplarRepository ejemplarRepository) {
+    public EjemplarService(
+        EjemplarRepository ejemplarRepository,
+        SitioDeRecoleccionRepository sitioDeRecoleccionRepository
+    ) {
         this.ejemplarRepository = ejemplarRepository;
+        this.sitioDeRecoleccionRepository = sitioDeRecoleccionRepository;
     }
 
     public List<EjemplarMinDTO> findAll() {
@@ -23,6 +31,19 @@ public  class EjemplarService {
     }
 
     public EjemplarDTO findByAdquisicionId(String anAdquisicionId) {
-        return ejemplarRepository.getEjemplarById(anAdquisicionId);
+        return ejemplarRepository.findDTOById(anAdquisicionId);
+    }
+
+    @Transactional
+    public Ejemplar save(EjemplarDTO anEjemplarDTO) {
+        Ejemplar ejemplarToSave = new Ejemplar();
+
+        ejemplarToSave.setRecolectadoPor(anEjemplarDTO.getRecolectadoPor());
+        ejemplarToSave.setFechaDeRecoleccion(anEjemplarDTO.getFechaDeRecoleccion());
+        ejemplarToSave.setSitioDeRecoleccion(sitioDeRecoleccionRepository.getReferenceById(anEjemplarDTO.getSitioDeRecoleccion().getId()));
+        ejemplarToSave.setProcedencia(anEjemplarDTO.getProcedencia());
+        ejemplarToSave.setObservaciones(anEjemplarDTO.getObservaciones());
+
+        return ejemplarRepository.save(ejemplarToSave);
     }
 }
