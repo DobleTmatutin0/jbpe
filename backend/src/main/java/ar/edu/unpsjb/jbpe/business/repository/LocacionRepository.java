417a4b88bcf7252c15b07875eaf59a689fb16a0d
DobleTmatutin0 <zz_00024@hotmail.com>
2026-09-22 18:31:04 -0300

    add repository for sitio de recoleccion and locacion

---
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/business/repository/LocacionRepository.java
@@ -0,0 +1,31 @@
+package ar.edu.unpsjb.jbpe.business.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unpsjb.jbpe.model.dto.LocacionDTO;
+import ar.edu.unpsjb.jbpe.model.entity.Locacion;
+
+@Repository
+
+public interface LocacionRepository extends JpaRepository<Locacion, Integer> {
+    // Default JpaRepository methods
+
+    // agregar busqueda de lista
+    // agregar busqueda por termino (typeahead)
+    @Query("""
+        SELECT new ar.edu.unpsjb.jbpe.model.dto.LocacionDTO(
+            l.id,
+            l.nombre,
+            l.locacionLvl,
+            lp.id,
+            lp.nombre
+        )
+        FROM Locacion AS l
+            LEFT JOIN l.locacionPadre AS lp
+        WHERE l.id = :aLocacionId
+    """)
+    LocacionDTO findDTOById(@Param("aLocacionId") Integer aLocacionId);
+}
---
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/business/repository/SitioDeRecoleccionRepository.java
@@ -0,0 +1,35 @@
+package ar.edu.unpsjb.jbpe.business.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unpsjb.jbpe.model.dto.SitioDeRecoleccionDTO;
+import ar.edu.unpsjb.jbpe.model.entity.SitioDeRecoleccion;
+
+@Repository
+
+public interface SitioDeRecoleccionRepository extends JpaRepository<SitioDeRecoleccion, Integer> {
+    // Default JpaRepository methods
+
+    // agregar busqueda de lista
+    // agregar busqueda por termino
+
+    @Query("""
+        SELECT new ar.edu.unpsjb.jbpe.model.dto.SitioDeRecoleccionDTO(
+            sdr.id,
+            sdr.nombre,
+            sdr.latitud,
+            sdr.longitud,
+            sdr.altitud,
+            l.id,
+            l.nombre,
+            sdr.descripcion
+        )
+        FROM SitioDeRecoleccion AS sdr
+            LEFT JOIN sdr.locacion AS l
+        WHERE sdr.id = :aSitioDeRecoleccionId
+    """)
+    SitioDeRecoleccionDTO findDTOById(@Param("aSitioDeRecoleccionId") Integer aSitioDeRecoleccionId);
+}
