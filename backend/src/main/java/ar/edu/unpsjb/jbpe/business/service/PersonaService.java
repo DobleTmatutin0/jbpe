eb274b4534098a865d54017db4542dc5a1233af5
DobleTmatutin0 <zz_00024@hotmail.com>
2026-09-23 17:44:28 -0300

    add save/create method

--- backend/src/main/java/ar/edu/unpsjb/jbpe/business/service/PersonaService.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/business/service/PersonaService.java
@@ -1,8 +1,12 @@
 package ar.edu.unpsjb.jbpe.business.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import ar.edu.unpsjb.jbpe.business.repository.PersonaRepository;
+import ar.edu.unpsjb.jbpe.model.dto.PersonaDTO;
+import ar.edu.unpsjb.jbpe.model.entity.Persona;
 
 @Service
 
@@ -13,6 +17,22 @@ public class PersonaService {
         this.personaRepository = personaRepository;
     }
 
+    public List<Persona> findAll() {
+        return personaRepository.findAll();
+    }
 
+    public  Persona findById(Integer aPersonaId) {
+        return personaRepository.findById(aPersonaId).orElse(null);
+    }
+
+    public Persona save(PersonaDTO aPersonaDTO) {
+        Persona personaToSave = new Persona();
+
+        personaToSave.setNombre(aPersonaDTO.getNombre());
+        personaToSave.setApellido(aPersonaDTO.getApellido());
+        personaToSave.setTipoDePersona(aPersonaDTO.getTipoDePersona());
+
+        return personaRepository.save(personaToSave);
+    }
 
 }
--- backend/src/main/java/ar/edu/unpsjb/jbpe/presenter/PersonaPresenter.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/presenter/PersonaPresenter.java
@@ -1,9 +1,17 @@
 package ar.edu.unpsjb.jbpe.presenter;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ar.edu.unpsjb.jbpe.Response;
 import ar.edu.unpsjb.jbpe.business.service.PersonaService;
+import ar.edu.unpsjb.jbpe.model.dto.PersonaDTO;
 
 @RestController
 @RequestMapping("persona")
@@ -15,6 +23,22 @@ public class PersonaPresenter {
         this.personaService = personaService;
     }
 
+    @GetMapping()
+    public ResponseEntity<Object> findAll() {
+        return Response.ok(personaService.findAll());
+    }
 
+    @GetMapping("/{aPersonaId}")
+    public ResponseEntity<Object> findById(@PathVariable("aPersonaId") Integer aPersonaId) {
+        return Response.ok(personaService.findById(aPersonaId));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> create(PersonaDTO aPersonaDTO) {
+        if(aPersonaDTO.getId() != null) {
+            Response.error(aPersonaDTO, "una nueva persona no puede tener id");
+        }
+        return Response.ok(personaService.save(aPersonaDTO));
+    }
 
 }
