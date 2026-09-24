6f3fddb53bbfd6d0dd358e2b3f471ecf34252891
DobleTmatutin0 <zz_00024@hotmail.com>
2026-09-23 21:18:17 -0300

    add save method (is not finished)

--- backend/src/main/java/ar/edu/unpsjb/jbpe/business/service/EventoService.java
+++ backend/src/main/java/ar/edu/unpsjb/jbpe/business/service/EventoService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import ar.edu.unpsjb.jbpe.business.repository.EventoRepository;
 import ar.edu.unpsjb.jbpe.model.dto.EventoDTO;
+import ar.edu.unpsjb.jbpe.model.entity.Evento;
 
 @Service
 
@@ -18,6 +19,17 @@ public class EventoService {
 
     public List<EventoDTO> findAll() {
         return eventoRepository.findAllDTO();
+    }
+
+    public Evento save(EventoDTO aEventoDTO) {
+        Evento eventoToSave = new Evento()
+
+        eventoToSave.set
+        eventoToSave.set
+        eventoToSave.set
+        eventoToSave.set
+
+        return eventoRepository.save(eventoToSave);
     }
 
 }
